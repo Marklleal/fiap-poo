e944a1387c5ab708dc03ee306c4fdc066b83b5a6
mkll <markllealsw@gmail.com>
2026-09-03 11:02:38 -0300

    feat: add constructor and method

--- fiaplibrary/src/br/com/fiaplibrary/main/MainSystem.java
+++ fiaplibrary/src/br/com/fiaplibrary/main/MainSystem.java
@@ -4,20 +4,19 @@ import br.com.fiaplibrary.model.Book;
 
 public class MainSystem {
     public static void main(String[] args) {
-        Book book1 = new Book();
-        book1.name = "Introduction to Python";
-        book1.totalBooks = 10;
-        book1.totalAvaibleBooks = 3;
+        // Using constructor to create new objects
+        Book introductionToPython = new Book("Introduction to Python", 10, 135);
+        Book introductionToOOP = new Book("Introduction to OOP", 5, 407);
 
-        Book book2 = new Book();
-        book2.name = "Introduction to OOP";
-        book2.totalBooks = 5;
-        book2.totalAvaibleBooks = 3;
+        // Guard clauses use
+        introductionToPython.bookRental(3);
+        introductionToOOP.bookRental(3);
 
         System.out.println("--- Fiap Library System ---");
-        System.out.println("Book name: " + book1.name +
-            "\nAvaible books: " + book1.totalAvaibleBooks);
-        System.out.println("Book name: " + book2.name +
-            "\nAvaible books: " + book2.totalAvaibleBooks);
+        System.out.println("Book name: " + introductionToPython.name +
+            "\nAvaible books: " + introductionToPython.totalAvailableBooks);
+
+        System.out.println("Book name: " + introductionToOOP.name +
+            "\nAvaible books: " + introductionToOOP.totalAvailableBooks);
     }
 }
--- fiaplibrary/src/br/com/fiaplibrary/model/Book.java
+++ fiaplibrary/src/br/com/fiaplibrary/model/Book.java
@@ -3,5 +3,22 @@ package br.com.fiaplibrary.model;
 public class Book {
     public String name;
     public int totalBooks;
-    public int totalAvaibleBooks;
+    public int totalAvailableBooks;
+    public int totalPages;
+
+    // Constructor declaration - It's not the same that method
+    public Book(String name, int totalBooks, int totalPages) {
+        this.name = name;
+        this.totalBooks = this.totalAvailableBooks = totalBooks;
+        this.totalPages = totalPages;
+    }
+
+    public void bookRental(int numBooks) {
+        if (numBooks > totalAvailableBooks) {
+            System.out.println("Out of stock!");
+            return; // It's our "exit" at this moment
+        }
+
+        totalAvailableBooks -= numBooks;
+    }
 }
