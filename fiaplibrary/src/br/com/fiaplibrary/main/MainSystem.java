05a29eadc9814dd1444786e88173cf9be8d824e2
mkll <markllealsw@gmail.com>
2026-09-21 19:17:46 -0300

    feat(fiaplibrary): add Patron usage in MainSystem demo

--- fiaplibrary/src/br/com/fiaplibrary/main/MainSystem.java
+++ fiaplibrary/src/br/com/fiaplibrary/main/MainSystem.java
@@ -1,7 +1,16 @@
 package br.com.fiaplibrary.main;
 
+import br.com.fiaplibrary.model.Patron;
+import br.com.fiaplibrary.model.Book;
+
 public class MainSystem {
     public static void main(String[] args) {
-        
+        Book introductionToPython = new Book("Introduction to Python", 10, 135);
+        Patron cleide = new Patron("Cleide", 0, 0, 50.0);
+
+        System.out.println(
+            cleide.getName() + " is renting the " +
+            introductionToPython.getName() + " book"
+        );
     }
 }
